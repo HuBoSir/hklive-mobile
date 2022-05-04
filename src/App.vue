7d53f0fbb5e67185144134a9b7db9a0e230d94f4
HuBoSir <212175267@qq.com>
2022-05-04 22:03:04 +0800

    引用了vant组件库和字体图标

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,21 @@
     <!--  路由出口  -->
     <router-view></router-view>
     <h1>黄科Live</h1>
+    <i class="hklive hklive-guanbi"></i>
+    <van-button type="primary">主要按钮</van-button>
+<van-button type="info">信息按钮</van-button>
+<van-button type="default">默认按钮</van-button>
+<van-button type="warning">警告按钮</van-button>
+<van-button type="danger">危险按钮</van-button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'App'
-
 }
 </script>
 
 <style lang="less">
-  *{}
+
 </style>
