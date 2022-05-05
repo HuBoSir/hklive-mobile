f417cf6b2f0c3257b12ae5c7a510e0c490e47fb1
HuBoSir <212175267@qq.com>
2022-05-05 09:34:23 +0800

    彻底初始化完毕

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,10 @@
   <div id="app">
     <!--  路由出口  -->
     <router-view></router-view>
-    <h1>黄科Live</h1>
-    <i class="hklive hklive-guanbi"></i>
+    <div class="dv">黄科Live</div>
+    <!-- <i class="hklive hklive-guanbi"></i> -->
     <van-button type="primary">主要按钮</van-button>
 <van-button type="info">信息按钮</van-button>
-<van-button type="default">默认按钮</van-button>
 <van-button type="warning">警告按钮</van-button>
 <van-button type="danger">危险按钮</van-button>
   </div>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import store from './store'
 import './styles/index.less'
 import Vant from 'vant'
 import 'vant/lib/index.css'
+//  动态设置 REM 基准值
+import 'amfe-flexible'
 
 Vue.use(Vant)
 
