dfe95ecdc6a7d33a5a085c132efc73d8d336e127
HuBoSir <212175267@qq.com>
2022-05-11 20:50:30 +0800

    初步完成详情页面

---
+++ src/views/article/index.vue
@@ -0,0 +1,344 @@
+<template>
+  <div class="article-container">
+    <!-- 导航栏 -->
+    <van-nav-bar
+      class="page-nav-bar"
+      left-arrow
+      title="黄科生活"
+      @click-left="$router.back()"
+    ></van-nav-bar>
+    <!-- /导航栏 -->
+
+    <div class="main-wrap">
+      <!-- 加载中 -->
+      <div v-if="loading" class="loading-wrap">
+        <van-loading
+          color="#3296fa"
+          vertical
+        >加载中</van-loading>
+      </div>
+      <!-- /加载中 -->
+
+      <!-- 加载完成-文章详情 -->
+      <div v-else-if="article.title" class="article-detail">
+        <!-- 文章标题 -->
+        <h1 class="article-title">{{ article.title }}</h1>
+        <!-- /文章标题 -->
+
+        <!-- 用户信息 -->
+        <van-cell class="user-info" center :border="false">
+          <van-image
+            class="avatar"
+            slot="icon"
+            round
+            fit="cover"
+            :src="article.aut_photo"
+          />
+          <div slot="title" class="user-name">{{ article.aut_name }}</div>
+          <div slot="label" class="publish-date">{{ article.pubdate | relativeTime }}</div>
+          <van-button
+          v-if="arctive.is_followed"
+          icon="plus"
+          class="follow-btn"
+          round
+          type="info"
+          >关注</van-button>
+        </van-cell>
+        <!-- /用户信息 -->
+
+        <!-- 文章内容 -->
+        <div
+          class="article-content markdown-body"
+          v-html="article.content"
+          ref="article-content"
+        >
+        </div>
+        <van-divider>正文结束</van-divider>
+        <!-- 文章评论列表 -->
+        <!-- /文章评论列表 -->
+        <!-- 底部区域 -->
+        <div class="article-bottom">
+          <van-button
+            class="comment-btn"
+            type="default"
+            round
+            size="small"
+            @click="isPostShow = true"
+          >写评论</van-button>
+          <van-icon
+            class="comment-icon"
+            name="comment-o"
+            :info="totalCommentCount"
+          />
+          <van-icon name="share" color="#777777"></van-icon>
+        </div>
+        <!-- /底部区域 -->
+
+        <!-- 发布评论 -->
+        <van-popup
+          v-model="isPostShow"
+          position="bottom"
+        >
+          <comment-post
+            :target="article.art_id"
+          />
+        </van-popup>
+        <!-- 发布评论 -->
+      </div>
+      <!-- /加载完成-文章详情 -->
+
+      <!-- 加载失败：404 -->
+      <div v-else-if="errStatus === 404" class="error-wrap">
+        <van-icon name="failure" />
+        <p class="text">该资源不存在或已删除！</p>
+      </div>
+      <!-- /加载失败：404 -->
+
+      <!-- 加载失败：其它未知错误（例如网络原因或服务端异常） -->
+      <div v-else class="error-wrap">
+        <van-icon name="failure" />
+        <p class="text">内容加载失败！</p>
+        <van-button
+          @click="loadArticle"
+        >点击重试</van-button>
+      </div>
+      <!-- /加载失败：其它未知错误（例如网络原因或服务端异常） -->
+    </div>
+
+    <!-- 评论回复 -->
+    <!--
+      弹出层是懒渲染的：只有在第一次展示的时候才会渲染里面的内容，之后它的关闭和显示都是在切换内容的显示和隐藏
+     -->
+    <van-popup
+      v-model="isReplyShow"
+      position="bottom"
+      style="height: 100%;"
+    >
+      <!--
+        v-if 条件渲染
+          true：渲染元素节点
+          false：不渲染
+       -->
+      <comment-reply
+        v-if="isReplyShow"
+        :comment="currentComment"
+        @close="isReplyShow = false"
+      />
+    </van-popup>
+    <!-- /评论回复 -->
+  </div>
+</template>
+
+<script>
+import { ImagePreview } from 'vant'
+let sgh = 'sfuhgfkwehfkie'
+sgh = 'shduwehfiowehaeh'
+console.log(sgh)
+const articleDate = {
+  title: '同样的数据我只写一次,加一段话显得我内容很多',
+  aut_name: '出来混讲视力',
+  aut_id: 0,
+  pubdate: new Date(),
+  comm_count: 1233,
+  aut_photo: require('@/assets/01.jpg'),
+  content: '<div><p>就假数据而言，我觉得我这个<b>假数据</b>造的特别有水准</div><div><img src="https://img01.yzcdn.cn/vant/apple-1.jpg" /><img src="https://img01.yzcdn.cn/vant/apple-2.jpg" /></div><div><p>字数凑得多，样式写的美观，可惜上线后这么完美的一条假数据就要再也见不到了，嘤嘤嘤</p></div>',
+  is_followed: false
+}
+export default {
+  name: 'ArticleIndex',
+  components: {},
+  props: {
+    articleId: {
+      type: [Number, String, Object],
+      required: true
+    }
+  },
+  data () {
+    return {
+      article: {}, // 文章详情
+      loading: true, // 加载中的 loading 状态
+      errStatus: 0, // 失败的状态码
+      followLoading: false,
+      totalCommentCount: 0,
+      isPostShow: false, // 控制发布评论的显示状态
+      commentList: [], // 评论列表
+      isReplyShow: false,
+      currentComment: {} // 当前点击回复的评论项
+    }
+  },
+  computed: {},
+  watch: {},
+  created () {
+    this.loadArticle()
+  },
+  mounted () {},
+  methods: {
+    async loadArticle () {
+      this.loading = true
+      setTimeout(() => {
+        this.article = articleDate
+        this.loading = false
+        setTimeout(() => {
+          this.previewImage()
+        }, 0)
+        //  初始化图片点击预览
+      }, 500)
+    },
+    previewImage () {
+      const articleContent = this.$refs['article-content']
+      const imgs = articleContent.querySelectorAll('img')
+      const images = []
+      imgs.forEach((img, index) => {
+        images.push(img.src)
+        img.onclick = () => {
+          ImagePreview({
+            images,
+            startPosition: index
+          })
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+@import "./github-markdown.css";
+
+.article-container {
+  .main-wrap {
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    background-color: #fff;
+  }
+  .article-detail {
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 92px;
+    bottom: 88px;
+    overflow-y: scroll;
+    background-color: #fff;
+    .article-title {
+      font-size: 40px;
+      padding: 50px 32px;
+      margin: 0;
+      color: #3a3a3a;
+    }
+
+    .user-info {
+      padding: 0 32px;
+      .avatar {
+        width: 70px;
+        height: 70px;
+        margin-right: 17px;
+      }
+      .van-cell__label {
+        margin-top: 0;
+      }
+      .user-name {
+        font-size: 24px;
+        color: #3a3a3a;
+      }
+      .publish-date {
+        font-size: 23px;
+        color: #b7b7b7;
+      }
+      .follow-btn {
+        width: 170px;
+        height: 58px;
+      }
+    }
+
+    .article-content {
+      padding: 55px 32px;
+      /deep/ p {
+        text-align: justify;
+      }
+    }
+  }
+
+  .loading-wrap {
+    padding: 200px 32px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #fff;
+  }
+
+  .error-wrap {
+    padding: 200px 32px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: #fff;
+    .van-icon {
+      font-size: 122px;
+      color: #b4b4b4;
+    }
+    .text {
+      font-size: 30px;
+      color: #666666;
+      margin: 33px 0 46px;
+    }
+    .retry-btn {
+      width: 280px;
+      height: 70px;
+      line-height: 70px;
+      border: 1px solid #c3c3c3;
+      font-size: 30px;
+      color: #666666;
+    }
+  }
+
+  .article-bottom {
+    position: fixed;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    box-sizing: border-box;
+    height: 88px;
+    border-top: 1px solid #d8d8d8;
+    background-color: #fff;
+    .comment-btn {
+      width: 282px;
+      height: 46px;
+      border: 2px solid #eeeeee;
+      font-size: 30px;
+      line-height: 46px;
+      color: #a7a7a7;
+    }
+    /deep/ .van-icon {
+      font-size: 40px;
+    }
+    .comment-icon {
+      top: 2px;
+      color: #777;
+      .van-info {
+        font-size: 16px;
+        background-color: #e22829;
+      }
+    }
+    .btn-item {
+      border: none;
+      padding: 0;
+      height: 40px;
+      line-height: 40px;
+      color: #777777
+    }
+    .collect-btn--collected {
+      color: #ffa500;
+    }
+    .like-btn--liked {
+      color: #e5645f;
+    }
+  }
+}
+</style>
